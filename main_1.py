4030a61f0067d8305ea9752a3d75064c1e0b8d8e
umairmayo80 <aliumer1080@gmail.com>
2024-07-18 23:55:07 +0500

    added polygon view images and polygon delete, image delete

--- main_1.py
+++ main_1.py
@@ -3,44 +3,93 @@ from tkintermapview import TkinterMapView
 from tkinter import filedialog, simpledialog, messagebox
 import os
 import tkinter as tk
-import time
 from PIL import Image, ImageTk
 from functools import partial
+from tkinter import ttk
 
 customtkinter.set_default_color_theme("green")
 customtkinter.set_appearance_mode("dark")
 
+# Define the button style
+style = ttk.Style()
+style.configure("TButton", font=("Arial", 12))
+
+# Configure the style for the "View Images" button
+style.configure("ViewImages.TButton", background="#2196F3", foreground="white")
+
+# Define the button colors
+view_images_button_color = "#2196F3"  # Material Design blue
+delete_polygon_button_color = "#F44336"  # Material Design red
 
 def display_files_in_marker(marker):
+    window = tk.Tk()
+    window.title("Marker Info")
+
     folder_path = marker.data.get("folder_path")
     message = f"Marker: {marker.text}\nCoordinates: {marker.position[0]}, {marker.position[1]}\n"
+
     if folder_path:
         files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
         if files:
             file_list = "\n".join(files)
-            messageFiles = f"Files:\n{file_list}"
+            message_files = f"Files:\n{file_list}"
         else:
-            messageFiles = "No files found in the folder."
+            message_files = "No files found in the folder."
     else:
-        messageFiles = "No folder path associated with this marker."
-    messagebox.showinfo(title="Marker Info", message=message+messageFiles)
+        message_files = "No folder path associated with this marker."
+
+    info_message = message + message_files
+    message_label = tk.Label(window, text=info_message)
+    message_label.pack()
+
+    delete_button = tk.Button(window, text="Delete Marker", command=lambda: delete_marker(marker))
+    delete_button.pack()
 
 
-def show_tooltip(widget, text):
-    x, y, _, _ = widget.bbox("all")
-    x += widget.winfo_rootx() + widget.winfo_width() // 2
-    y += widget.winfo_rooty() + widget.winfo_height() // 2
+def delete_marker(marker):
+    marker.delete()
+    messagebox.showwarning("Marker deleted", "Marker deleted")
 
-    tooltip = tk.Toplevel(widget)
+
+
+def display_full_image(image_path):
+    # Create a new window to display the full image
+    window = tk.Toplevel()
+    window.title(image_path)
+
+    # Load the full image
+    image = Image.open(image_path)
+    photo = ImageTk.PhotoImage(image)
+
+    # Create a label to display the full image
+    label = tk.Label(window, image=photo)
+    label.image = photo  # Keep a reference to prevent garbage collection
+    label.pack()
+
+
+def on_hover(label, text, event):
+    label.config(cursor="hand2")
+    x = label.winfo_rootx()
+    y = label.winfo_rooty() - 20
+
+    tooltip = tk.Toplevel(label)
     tooltip.wm_overrideredirect(True)
     tooltip.wm_geometry(f"+{x}+{y}")
-    
-    label = tk.Label(tooltip, text=text, background="#ffffe0", relief="solid", borderwidth=1)
-    label.pack(padx=5, pady=2)
 
-def hide_tooltip(widget):
-    for child in widget.winfo_children():
+    label_ttp = tk.Label(tooltip, text=text, background="#ffffe0", relief="solid", borderwidth=1)
+    label_ttp.pack(padx=5, pady=2)
+
+
+def on_leave(label, event):
+    label.config(cursor="")
+    for child in label.winfo_children():
         child.destroy()
+
+
+def delete_polygon(polygon):
+    polygon.delete()
+    messagebox.showwarning("Deleted Polygon", "Deleted polygon")
+
 
 class App(customtkinter.CTk):
     APP_NAME = "Vectrino Mapa"
@@ -117,7 +166,7 @@ class App(customtkinter.CTk):
                 if new_marker:
                     new_marker.data = {"folder_path": folder_path}
                     print(f"Added marker at: {coords}")
-    
+
     def polygon_add_marker_event(self, args):
         marker = self.map_widget.set_marker(args[0], args[1])
         print(f"Added marker at: {args[0], args[1]}")
@@ -129,96 +178,108 @@ class App(customtkinter.CTk):
             self.map_widget.add_left_click_map_command(callback_function=None)
 
             selected_images = filedialog.askopenfilenames(title="Select Images",
-                                                        filetypes=(("Image files", "*.jpg;*.jpeg;*.png"),))
+                                                          filetypes=(("Image files", "*.jpg;*.jpeg;*.png"),))
             if selected_images:
                 messagebox.showinfo("Selected Images", f"Selected Images:\n{selected_images}")
-                polygon.data = {"selected_images" : selected_images}
+                polygon.data = {"selected_images": selected_images}
             else:
                 messagebox.showwarning("No Images Selected", "No images were selected.")
-        
+            # rest the marker polygons
+            for marker in self.marker_list:
+                marker.delete()
+            self.marker_list = []
+            self.current_polygon_markers_count = 0
 
     def polygon_click(self, polygon):
-        if "selected_images" in polygon.data:
+        if polygon.data and "selected_images" in polygon.data:
             selected_images = polygon.data["selected_images"]
             if selected_images:
-                # Create a new window to display the images
+                # Create a new window to display the selected images
                 window = tk.Toplevel(self)
                 window.title("Selected Images")
 
-                  # Calculate the number of rows and columns for thumbnails
-                num_images = len(selected_images)
-                num_columns = 4  # Adjust the number of columns as desired
-                num_rows = (num_images + num_columns - 1) // num_columns
+                # Create a label to show the selected images as text
+                images_label = tk.Label(window, text="Selected Images:\n" + "\n".join(selected_images), font=("Arial", 12))
+                images_label.pack(padx=20, pady=20)
 
-                 # Calculate the canvas width and height based on thumbnail size and number of rows/columns
-                thumbnail_width = 300  # Adjust the thumbnail width as needed
-                thumbnail_height = 300  # Adjust the thumbnail height as needed
-                canvas_width = num_columns * thumbnail_width
-                canvas_height = num_rows * thumbnail_height
-            
-                
-                # Create a scrollable canvas to hold the image thumbnails
-                  # Create a scrollable canvas to hold the image thumbnails
-                canvas = tk.Canvas(window, width=canvas_width, height=canvas_height, borderwidth=0, background="#ffffff")
-                scrollbar = tk.Scrollbar(window, orient="vertical", command=canvas.yview)
-                canvas.configure(yscrollcommand=scrollbar.set)
-                
-                # Create a frame to hold the image thumbnails
-                frame = tk.Frame(canvas, background="#ffffff")
-                frame.bind("<Configure>", lambda event: canvas.configure(scrollregion=canvas.bbox("all")))
-                
-                canvas.create_window((0, 0), window=frame, anchor="nw")
-                canvas.pack(side="left", fill="both", expand=True)
-                scrollbar.pack(side="right", fill="y")
-                
-                # Load and display the image thumbnails
-                for i, image_path in enumerate(selected_images):
-                    image = Image.open(image_path)
-                    thumbnail_size = (300, 300)  # Adjust the thumbnail size as needed
-                    image.thumbnail(thumbnail_size)
-                    photo = ImageTk.PhotoImage(image)
-                    
-                     # Create a clickable label for the thumbnail
-                    label = tk.Label(frame, image=photo)
-                    label.image = photo  # Keep a reference to prevent garbage collection
-                    
-                    # Bind a callback function to the label click event
-                    callback = partial(self.display_full_image, image_path)
-                    label.bind("<Button-1>", lambda event, cb=callback: cb())
+                # Create a frame to hold the buttons
+                buttons_frame = tk.Frame(window)
+                buttons_frame.pack(padx=20, pady=10)
 
-                    # Create tooltips for image names
-                    label.bind("<Enter>", lambda event, text=image_path: show_tooltip(label, text))
-                    label.bind("<Leave>", lambda event: hide_tooltip(label))
-                    
-                    # label.pack(side="left", padx=10, pady=10)
+                # Create the View Images button
+                view_images_button = tk.Button(buttons_frame, text="View Images", font=("Arial", 12),
+                                               bg=view_images_button_color,
+                                               command=lambda: self.view_images(selected_images))
+                view_images_button.pack(side="left", padx=5)
 
-                     # Calculate the grid column and row positions
-                    col = i % num_columns
-                    row = i // num_columns
-                    
-                    # Use grid to position the label in the frame
-                    label.grid(row=row, column=col, padx=10, pady=10)
-
+                # Create the Delete Polygon button
+                delete_polygon_button = tk.Button(buttons_frame, text="Delete Polygon", font=("Arial", 12),
+                                                  command=lambda: delete_polygon(polygon))
+                delete_polygon_button.pack(side="left", padx=5)
             else:
                 messagebox.showwarning("No Images Selected", "No images were selected.")
         else:
             messagebox.showwarning("No Images Selected", "No images were selected.")
 
+    def view_images(self, selected_images):
+        if selected_images:
+            # Create a new window to display the images
+            window = tk.Toplevel(self)
+            window.title("Selected Images")
 
-    def display_full_image(self,  image_path):
-        # Create a new window to display the full image
-        window = tk.Toplevel()
-        window.title(image_path)
-        
-        # Load the full image
-        image = Image.open(image_path)
-        photo = ImageTk.PhotoImage(image)
-        
-        # Create a label to display the full image
-        label = tk.Label(window, image=photo)
-        label.image = photo  # Keep a reference to prevent garbage collection
-        label.pack()
+            # Calculate the number of rows and columns for thumbnails
+            num_images = len(selected_images)
+            num_columns = 4  # Adjust the number of columns as desired
+            num_rows = (num_images + num_columns - 1) // num_columns
 
+            # Calculate the canvas width and height based on thumbnail size and number of rows/columns
+            thumbnail_width = 300  # Adjust the thumbnail width as needed
+            thumbnail_height = 300  # Adjust the thumbnail height as needed
+            canvas_width = (thumbnail_width * num_columns) + 100
+            canvas_height = 800
+
+            # Create a scrollable canvas to hold the image thumbnails
+            canvas = tk.Canvas(window, width=canvas_width, height=canvas_height, borderwidth=0, background="#ffffff")
+            scrollbar = tk.Scrollbar(window, orient="vertical", command=canvas.yview)
+            canvas.configure(yscrollcommand=scrollbar.set)
+
+            # Create a frame to hold the image thumbnails
+            frame = tk.Frame(canvas, background="#ffffff")
+            frame.bind("<Configure>", lambda event: canvas.configure(scrollregion=canvas.bbox("all")))
+
+            canvas.create_window((0, 0), window=frame, anchor="nw")
+            canvas.pack(side="left", fill="both", expand=True)
+            scrollbar.pack(side="right", fill="y")
+
+            # Bind the mouse wheel event to the canvas for scrolling using scroll button
+            frame.bind("<MouseWheel>", lambda event: canvas.yview_scroll(int(-1 * (event.delta / 120)), "units"))
+
+            # Load and display the image thumbnails
+            for i, image_path in enumerate(selected_images):
+                image = Image.open(image_path)
+                thumbnail_size = (thumbnail_width, thumbnail_height)  # Adjust the thumbnail size as needed
+                image.thumbnail(thumbnail_size)
+                photo = ImageTk.PhotoImage(image)
+
+                # Create a clickable label for the thumbnail
+                label = tk.Label(frame, image=photo)
+                label.image = photo  # Keep a reference to prevent garbage collection
+
+                # Bind a callback function to the label click event
+                callback = partial(display_full_image, image_path)
+                label.bind("<Button-1>", lambda event, cb=callback: cb())
+
+                label.bind("<Enter>", partial(on_hover, label, image_path))
+                label.bind("<Leave>", partial(on_leave, label))
+
+                # Calculate the grid column and row positions
+                col = i % num_columns
+                row = i // num_columns
+
+                # Use grid to position the label in the frame
+                label.grid(row=row, column=col, padx=10, pady=10)
+        else:
+            messagebox.showwarning("No Images Selected", "No images were selected.")
 
     def change_layer(self):
         self.current_layer_index = (self.current_layer_index + 1) % len(self.layer_urls)
@@ -246,3 +307,6 @@ class App(customtkinter.CTk):
 if __name__ == "__main__":
     app = App()
     app.start()
+
+# todo; correct tool tip position, add method to delete the marker and polygon,
+# save app state
